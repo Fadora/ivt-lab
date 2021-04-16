ea9ff6e5ae4f761851882a70bca4c1ba6a4e3983
Fadora <fabiandora26@gmai.com>
2021-04-16 15:49:49 +0200

    modified-branch-B2

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -6,7 +6,7 @@ import java.util.Random;
 * Class storing and managing the torpedoes of a ship
 *
 * (Deliberately contains bugs.)
-*/ // differently modified branch B
+*/ 
 public class TorpedoStore {
 
   // rate of failing to fire torpedos [0.0, 1.0]
